760dacf0c024d6266fd0737b7020cd582d182168
hermitAT <ahthornev2@gmail.com>
2021-02-03 23:55:53 -0800

    Edit style, add dropdown endpoints

--- src/components/NavBar/Dropdown.js
+++ src/components/NavBar/Dropdown.js
@@ -6,8 +6,14 @@ import "./Dropdown.scss";
 
 export default function Dropdown(props) {
   return (
-    <ul className="top-level-menu">
+    <ul className="menu-level__top">
       <li><FontAwesomeIcon icon="bars" size="3x" />
+      <ul className='menu-level__second'>
+        <li>About</li>
+        <li>Portfolio</li>
+        <li>Gaming</li>
+        <li>Contact</li>
+      </ul>
       </li>
     </ul>
   )

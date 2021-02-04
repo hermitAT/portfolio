7bcddf579ebedc2f0583ff749526dd5002000a21
hermitAT <ahthornev2@gmail.com>
2021-02-03 17:25:06 -0800

    Quick change to icons on navbar

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import Dropdown from "./Dropdown";
-import sprites from "./sprites.png";
 import "./index.scss";
 
 export default function NavBar(props) {
@@ -9,7 +8,7 @@ export default function NavBar(props) {
   return (
     <nav className="navbar">
       <div className="navbar__aside">
-        <img className="navbar__pic" src={sprites} alt="profile pic" />
+        <img className="navbar__pic" src="https://media.giphy.com/media/TZco470UACpNK/source.gif" alt="profile pic" />
         <h1 className="navbar__title">Adam Thorne</h1>
       </div>
       <Dropdown />

e9885e5add4d5174742c107d1fce27762b641631
hermitAT <ahthornev2@gmail.com>
2021-02-04 23:13:39 -0800

    Edit about intro page, next step edu page

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import useVisualMode from "hooks/useVisualMode";
+import Intro from "./Intro";
 
 import "./index.scss";
 
@@ -17,13 +18,7 @@ export default function Home() {
     <main className="about">
       <section className="about-container">
         {mode === INTRO && (
-          <>
-          <img src="https://scontent.fyvr4-1.fna.fbcdn.net/v/t1.0-9/90174233_10163067610045534_410187875192143872_o.jpg?_nc_cat=108&ccb=2&_nc_sid=09cbfe&_nc_ohc=XpjF01Vw_E8AX9RZQkZ&_nc_ht=scontent.fyvr4-1.fna&oh=ed0076cf41ab481242a73bd96eef27d7&oe=6041CE15" alt="profile pic" />
-          <div className="about-desc">
-            <h3>Hi! I'm Adam Thorne,</h3>
-            <h4>a full-stack web developer living in Victoria, BC.</h4>
-          </div>
-          </>
+          <Intro />
         )}
 
       </section>

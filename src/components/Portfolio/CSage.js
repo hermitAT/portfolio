e340e29ec21f496c2be57bfa0a1f1f0cc2b54bb2
hermitAT <ahthornev2@gmail.com>
2021-02-10 21:16:20 -0800

    Complete DevTips project page

--- src/components/Portfolio/CSage.js
+++ src/components/Portfolio/CSage.js
@@ -14,12 +14,13 @@ export default function CSage(props) {
         <FontAwesomeIcon onClick={props.onNext} icon="forward" size="2x" />
       </div>  
       <div className="portfolio-details">
-        <img id="csage-logo" src={logo} alt="sage logo" />
-        <p>CocktailSage - a cocktail app with a fluid and intuitive user experience for browsing, creating and finding recipes with particular characteristics.</p>
+        <img id="logo" src={logo} alt="sage logo" />
+        <p>CocktailSage was the capstone project during the Lighthouse Labs bootcamp. With a fluid and intuitive user experience for browsing, creating and searching for cocktail recipes with particular characteristics.</p>
+        <p>With a detailed database of ingredients, strengths, flavours and categories, recipes can be sorted and viewed based on all of the different attributes possible.</p>
         <p>Noteworthy and unique features include our calculated alcohol strength percentage (based on the ingredients used) and the 'reMix' feature, where users can use an existing recipe as a template to add their own twist to it.</p>
-        <p>TECH STACK: React.js, React Router and SASS for the front-end, with Ruby on Rails for the back-end.</p>
-        <p>TEAM MEMBERS: Pavel Piatetskii and Molly Sutthasilp</p>
         <img id="csage" src={gif} alt="cocktail sage" />
+        <p>TECH STACK: React.js, React Router and SASS for the front-end, with Ruby on Rails and PSQL for the back-end.</p>
+        <p>TEAM MEMBERS: Pavel Piatetskii and Molly Sutthasilp.</p>
       </div>
       <div className="portfolio-buttons">
         <FontAwesomeIcon onClick={props.onBack} icon="backward" size="2x" />
--- src/components/Portfolio/DevTips.js
+++ src/components/Portfolio/DevTips.js
@@ -1,14 +1,27 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
-import image from "./images/devtips.png";
+import logo from "./images/devtipslogo.png";
+import gif from "./images/devtips.gif";
 
 import "./index.scss";
 
 export default function DevTips(props) {
   return (
     <section className="portfolio-project">
-      <img id="devtips" src={image} alt="devtips" />
+      <div className="portfolio-buttons">
+        <FontAwesomeIcon onClick={props.onBack} icon="backward" size="2x" />
+        <FontAwesomeIcon onClick={props.onNext} icon="forward" size="2x" />
+      </div> 
+      <div className="portfolio-details">
+        <img id="logo" src={logo} alt="devtips logo" />
+        <p>DevTips was developed as a midterm project during Lighthouse Labs' bootcamp, designed for use by software and web developers to share tips, links and other helpful resources.</p>
+        <p>Users can browse or search through resources based on associated hashtags, and add their own resources split in several different categories.</p>
+        <p>Has support for YouTube, and also support for code snippets that can be viewed raw, copied or exported.</p>
+        <img id="devtips" src={gif} alt="devtips" />
+        <p>TECH STACK: Express.js using an EJS view engine and PSQL for the database.</p>
+        <p>TEAM MEMBERS: Pavel Piatetskii and Jeremy Dombrowski.</p>
+      </div>
       <div className="portfolio-buttons">
         <FontAwesomeIcon onClick={props.onBack} icon="backward" size="2x" />
         <FontAwesomeIcon onClick={props.onNext} icon="forward" size="2x" />

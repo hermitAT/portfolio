5f05b5be082a8a22ef34cfa97374074e44546671
hermitAT <ahthornev2@gmail.com>
2021-02-15 18:35:11 -0800

    Change text and add react-text-loop

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,103 +1,29 @@
-import React, { useEffect } from "react";
+import React from "react";
+import TextLoop from "react-text-loop";
 
 import Dropdown from "./Dropdown";
 import "./index.scss";
 
 export default function NavBar(props) {
 
-  // ——————————————————————————————————————————————————
-  // TextScramble
-  // ——————————————————————————————————————————————————
-  
-  useEffect(() => {
-
-    class TextScramble {
-      constructor(el) {
-        this.el = el
-        this.chars = '!<>-_\\/[]{}—=+*^?#________'
-        this.update = this.update.bind(this)
-      }
-      setText(newText) {
-        const oldText = this.el.innerText
-        const length = Math.max(oldText.length, newText.length)
-        const promise = new Promise((resolve) => this.resolve = resolve)
-        this.queue = []
-        for (let i = 0; i < length; i++) {
-          const from = oldText[i] || ''
-          const to = newText[i] || ''
-          const start = Math.floor(Math.random() * 40)
-          const end = start + Math.floor(Math.random() * 40)
-          this.queue.push({ from, to, start, end })
-        }
-        cancelAnimationFrame(this.frameRequest)
-        this.frame = 0
-        this.update()
-        return promise
-      }
-      update() {
-        let output = ''
-        let complete = 0
-        for (let i = 0, n = this.queue.length; i < n; i++) {
-          let { from, to, start, end, char } = this.queue[i]
-          if (this.frame >= end) {
-            complete++
-            output += to
-          } else if (this.frame >= start) {
-            if (!char || Math.random() < 0.28) {
-              char = this.randomChar()
-              this.queue[i].char = char
-            }
-            output += `<span class="dud">${char}</span>`
-          } else {
-            output += from
-          }
-        }
-        this.el.innerHTML = output
-        if (complete === this.queue.length) {
-          this.resolve()
-        } else {
-          this.frameRequest = requestAnimationFrame(this.update)
-          this.frame++
-        }
-      }
-      randomChar() {
-        return this.chars[Math.floor(Math.random() * this.chars.length)]
-      }
-    }
-
-    const phrases = [
-      'full-stack developer',
-      'bartender and mixologist',
-      'competitive CCG player',
-      'pokemon fanboy and collector',
-      'penalty kill specialist',
-      'drummer and music snob',
-      'ramen enthusiast',
-      'NHL superfan',
-      'avid manga reader'
-    ];
-  
-    const el = document.querySelector('.text');
-    const fx = new TextScramble(el);
-  
-    let counter = 0;
-    const next = () => {
-      fx.setText(phrases[counter]).then(() => {
-        setTimeout(next, 30300)
-      });
-      counter = (counter + 1) % phrases.length;
-    };
-
-    next();
-
-  }, []);
 
   return (
     <nav className="navbar">
       <div className="navbar__aside">
         <img className="navbar__pic" src="https://media.giphy.com/media/TZco470UACpNK/source.gif" alt="profile pic" />
         <h1 className="navbar__title">Adam Thorne,</h1>
-        <h2 className="text"> </h2>
+        <h2 className="text">
+          <TextLoop interval={4000}>
+            <span>full-stack developer</span>
+            <span>bartender and mixologist</span>
+            <span>artist and musician</span>
+            <span>barista extrordinaire</span>
+            <span>competitive CCG player</span>
+            <span>NHL superfan</span>
+            <span>avid manga reader</span>
+            <span>human Pokedex</span>
+          </TextLoop>{" "}
+        </h2>
       </div>
       <Dropdown />
     </nav>

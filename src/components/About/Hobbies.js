ceb014d7d0afbb3907f1c3355511f524b0e872d5
Adam Thorne <70076508+hermitAT@users.noreply.github.com>
2023-09-07 09:47:53 -0700

    Update Hobbies.js

--- src/components/About/Hobbies.js
+++ src/components/About/Hobbies.js
@@ -10,11 +10,12 @@ export default function Hobbies(props) {
   return (
     <>
     <section className="about-container">
-      <p>I play the drums, dabble in pixel art, have an endless list of things to watch and read, and love hockey - go Flyers!</p>
-      <p>I will play any game I can get my hands on, and have some background in competitive gaming and eSports. Ask me about collectible card games and I would never stop talking!</p>
-      <div className="about-intro">
+      <p>I love a good story. I'm an avid reader, huge fan of movies and TV, and will play any video game of any genre if the narrative is engaging enough.</p>
+      <p>I am also highly competitive! I  have played in tournaments and conferences all across western Canada and the US for games like Magic: the Gathering and Hearthstone, and I love to compete (even if I'm not very good).</p>
+      <p>I bring this competitive nature to sports as well, where I've recently gotten into golf and played in my first Men's tournament in the summer of 2023. I also play hockey and love to watch and play fantasy sports of all kinds.</p>  
+    <div className="about-intro">
         <img src={player} alt="nhl player" />
-        <h3>A short summary doesn't do enough to describe who I am and what I love, so please <Link to="/contact">CONNECT</Link> with me if you want to know more!</h3>
+        <h3>A short summary doesn't do enough to describe who I am and what I love, so please <Link to="/contact">CONNECT</Link> with me if you want to chat!</h3>
       </div>
       <div className="about-buttons">
         <FontAwesomeIcon onClick={props.onBack} icon="caret-square-left" size="2x" />
